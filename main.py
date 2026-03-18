4ca60dfcfdedc61b2f87b64ef7e17607e381a462
Jared Rudolph <jkrudolph@outlook.com>
2026-03-18 12:49:52 -0400

    feat: update main.py to import and run pipeline and optimizer functions correctly

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from dotenv import load_dotenv
 
-from macro_stress_pipeline.pipeline import run
+from macro_stress_ml.optimizer import run as run_optimizer
+from macro_stress_pipeline.pipeline import run as run_pipeline
 
 load_dotenv()
 
 if __name__ == "__main__":
-    run()
+    run_pipeline()
+    run_optimizer()
